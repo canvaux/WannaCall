819806be3aedf84a61bad25713807261189c7803
dheeraj1429 <dheerajsingh1429@gmail.com>
2022-02-25 16:01:07 +0530

    changes done

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const googleTrns = document.getElementById('google_translate_element');
+const loginButtonElm = document.querySelector('.login_button_div');
 
 function googleTranslateElementInit() {
    google.translate.TranslateElement({ pageLanguage: 'en' }, googleTrns);
@@ -19,9 +20,39 @@ const getTheDomElm = function () {
 
    const childNodes = getChildElm(skiptranslateELm);
    childNodes[1].remove();
+
+   // googTeComboElm.addEventListener('change', function (event) {
+   //    let value = event.target.value;
+
+   //    console.log(value);
+
+   //    // if (value === 'cs') {
+   //    //    loginButtonElm.style.fontSize = '10px';
+   //    // } else if (value === 'hr') {
+   //    //    loginButtonElm.style.fontSize = '11px';
+   //    // } else if (value === 'af') {
+   //    //    loginButtonElm.style.fontSize = '11px';
+   //    // } else if (value === 'am') {
+   //    //    loginButtonElm.style.fontSize = '14px';
+   //    // } else if (value === 'ar') {
+   //    //    loginButtonElm.style.fontSize = '12px';
+   //    // } else if (value === 'be') {
+   //    //    loginButtonElm.style.fontSize = '14px';
+   //    // } else if (value === 'cs') {
+   //    //    loginButtonElm.style.fontSize = '12px';
+   //    // } else if (value === 'my') {
+   //    //    loginButtonElm.style.fontSize = '12px';
+   //    // } else if (value === 'ca') {
+   //    //    loginButtonElm.style.fontSize = '12px';
+   //    // } else if (value === 'bg') {
+   //    //    loginButtonElm.style.fontSize = '12px';
+   //    // }
+
+   //    console.log(valueInnerText);
+   // });
 };
 
-// 1 -> change first link url -> http://yourwebsitename.com/Wanncall;
+// 1 -> change first link url -> http://yourwebsitename.com/index;
 // 2 -> change your localhost url -> http://yourwebsitename.com/landing_page;
 
 const storeData = function () {
